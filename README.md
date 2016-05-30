16f60348eabd1a3da9cca3b889671f71e101e077
wp13000 <meetupwp13@applh.com>
2016-05-30 22:02:28 +0000

    0530-22:02:28\n M README.md  M index.php

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # marspack
+
 Marseille WordPress Meetup Plugin Pack
+
+
+## note
+
+This file if for github repository description
+
--- index.php
+++ index.php
@@ -1,7 +1,19 @@
 <?php
 /*
-
 Plugin Name: MarsPack
-
 */
+
+if ( !defined('WPINC') ) 
+{
+    exit;
+}
+
+// PLUGIN PATH
+$curdir=__DIR__;
+
+// LOAD CLASS DECLARATION
+require_once("$curdir/MarsPack.php");
+
+// CREATE AN OBJECT FROM MarsPack CLASS
+$objMarsPack = new MarsPack;
 
