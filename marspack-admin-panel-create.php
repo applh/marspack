ea94e18e6d6afa88619bfb75333a91182fc3ebc2
wp13000 <meetupwp13@applh.com>
2016-06-11 23:36:10 +0000

    AUTO 0611-23:36:10\n M marspack-admin-panel-create.php

--- marspack-admin-panel-create.php
+++ marspack-admin-panel-create.php
@@ -6,3 +6,9 @@ if (!is_object($wpdb)) exit;
 
 ?>
 <h3>CREATE A NEW TABLE</h3>
+<div>
+    <form>
+        <input type="text" name="tableName" placeholder="TABLE NAME" required/>
+        <button>CREATE NEW TABLE</button>    
+    </form>
+</div>
