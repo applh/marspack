c279eac9fcd3a71f29ed19d0581b5b2cba76275c
wp13000 <meetupwp13@applh.com>
2016-06-12 11:49:33 +0000

    AUTO 0612-11:49:33\n M marspack-admin-panel-create.php

--- marspack-admin-panel-create.php
+++ marspack-admin-panel-create.php
@@ -4,14 +4,28 @@ global $wpdb;
 
 if (!is_object($wpdb)) exit;
 
+$requestDB = get_option("marspackDB", "");
+
+$idForm = getInput("idForm");
+if ($idForm == "createDB")
+{
+   $requestDB = getInput("requestDB");
+
+    // SAVE THE CONTENT
+    update_option("marspackDB", $requestDB);
+
+}
 ?>
 <h3>CREATE A NEW TABLE</h3>
 <div>
     <form>
-        <input type="text" name="tableName" placeholder="TABLE NAME" required/>
         <br>
-        <textarea name="commands" rows="10" cols="80"></textarea>
+        <textarea name="requestDB" rows="10" cols="80"><?php echo $requestDB; ?></textarea>
         <br>
-        <button>CREATE NEW TABLE</button>    
+        <button>CREATE NEW TABLE</button>
+        <input type="hidden" name="idForm" value="createDB">
+        <div class="feedback">
+            <?php echo $createDBfeedback?>
+        </div>
     </form>
 </div>
